eeb502dd566dcf0db2e07d4ae8f6fea771cd9f74
Andrii Kovalchuk <tieorange@gmail.com>
2026-02-25 19:54:53 +0200

    Updates

---
+++ assets/Queen - We Will Rock You-C53cixnM.js
@@ -0,0 +1,1 @@
+const e="/music-retro-game-react/assets/Queen%20-%20We%20Will%20Rock%20You-D_xNgf7c.mp3";export{e as default};
---
+++ assets/bandana-HZrHRCsa.js
@@ -0,0 +1,1 @@
+const a="/music-retro-game-react/assets/bandana-3SFwP-bP.mp3";export{a as default};
